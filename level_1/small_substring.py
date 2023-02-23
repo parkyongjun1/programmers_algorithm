8a77d32dafa704ba37c94dfb5b296bafebd1eb51
parkyongjun1 <qkr2938@gmail.com>
2023-02-23 11:08:04 +0900

    23.02.15

---
+++ level_1/small_substring.py
@@ -0,0 +1,23 @@
+# 크기가 작은 부분문자열
+# practive problem
+# 2023.02.15
+# 14:30 ~ 14:45 (15 min)
+# 후기 : 문제를 이해하고 어렵지 않게 해결 할 수 있었음. 
+
+
+def solution(t, p):
+    answer = 0
+    check_num = 0
+    check_cnt = 0
+    for  i in range(len(t)):
+        check_len = len(p)
+        if i+check_len > len(t):
+            pass
+        else:
+            check_num = int(t[i:i+check_len])
+            if check_num <= int(p):
+                check_cnt += 1
+                answer = check_cnt
+                
+        
+    return answer
